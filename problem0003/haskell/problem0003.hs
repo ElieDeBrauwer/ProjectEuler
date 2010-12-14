1e182406120c1e9a90b3992995f0aab4cec0fe59
Elie De Brauwer <elie@de-brauwer.be>
2010-12-14 19:39:06 +0100

    Adding haskell solution for problem 3

---
+++ problem0003/haskell/problem0003.hs
@@ -0,0 +1,14 @@
+{-
+Problem: http://projecteuler.net/index.php?section=problems&id=3
+Author: Elie De Brauwer <elie @ de-brauwer.be>
+License: Simplified BSD
+Date: December 14, 2010
+-}
+
+{- n = candidate divisor, a = divident -}
+factor :: Integer -> Integer -> [Integer]
+factor _ 1 = []
+factor n a = if (a `mod` n == 0) then n : factor n (a `div` n) else factor (n+1) a
+
+problem0003 :: [Integer]
+problem0003 = factor 2 600851475143
