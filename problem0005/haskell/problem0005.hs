9c53d8dd543962ecc0db55659e4673f761890bc7
Elie De Brauwer <elie@de-brauwer.be>
2010-12-26 19:49:10 +0100

    Adding haskell solution for problem 5

---
+++ problem0005/haskell/problem0005.hs
@@ -0,0 +1,16 @@
+{-
+Problem: http://projecteuler.net/index.php?section=problems&id=4
+Author: Elie De Brauwer <elie @ de-brauwer.be>
+License: Simplified BSD
+Date: December 26, 2010
+-}
+
+
+{- canBeDivided checks if an integer can be divided by all values lesser 
+   than a given argument-}
+canBeDivided :: Integer -> Integer -> Bool
+canBeDivided x 1 = True
+canBeDivided x y = if x `mod` y == 0 then canBeDivided x (y-1) else False
+
+
+main = print $  head [x*20 | x <- [1..], canBeDivided (x*20)  20 ]
